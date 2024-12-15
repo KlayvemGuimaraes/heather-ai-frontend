75db78133f96a2ad5bcae5cc34494e290f6d0869
KlayvemG <klayvemguik@gmail.com>
2024-12-14 21:32:45 -0300

    create sideBar

--- Pleasant/src/components/SideBar/SideBar.tsx
+++ Pleasant/src/components/SideBar/SideBar.tsx
@@ -1,20 +1,73 @@
 import React from 'react';
-import styles from './Sidebar.module.css';
+import styles from './SideBar.module.css';
+// import { FaTachometerAlt, FaUserCircle, FaMoneyBillAlt, FaUserTie, FaCog, FaQuestion } from 'react-icons/fa';
+import Chart from "../../assets/iconsSideBar/Chart.svg"
+import Buy from "../../assets/iconsSideBar/Buy.svg"
+import Document from "../../assets/iconsSideBar/Document.svg"
+import Chat from "../../assets/iconsSideBar/Chat.svg"
+import Setting from "../../assets/iconsSideBar/Setting.svg"
+import Wallet from "../../assets/iconsSideBar/Wallet.svg"
+import Profile from "../../assets/iconsSideBar/Profile.svg"
+import infoSquare from "../../assets/iconsSideBar/infoSquare.svg"
 
 const Sidebar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <h3>Menu</h3>
+        <img src="../../src/assets/logo/LogoPleasant.svg" alt="Logo Pleasant" />
       </div>
       <div className={styles.content}>
         <ul>
-          <li><a href="#">Dashboard</a></li>
-          <li><a href="#">My Account</a></li>
-          <li><a href="#">Transactions</a></li>
-          <li><a href="#">Customer Service</a></li>
-          <li><a href="#">Settings</a></li>
-          <li><a href="#">Help</a></li>
+          <h3>Menu</h3>
+          <li>
+            <a href="#">
+              <img src={Chart} className={styles.icon} />
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Buy} className={styles.icon} />
+              My Account
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Document} className={styles.icon} />
+              Transactions
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Chat} className={styles.icon} />
+              Customer Service
+            </a>
+          </li>
+          <h3>Others</h3>
+          <li>
+            <a href="#">
+              <img src={Setting} className={styles.icon} />
+              Settings
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Wallet} className={styles.icon} />
+              Sales
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={Profile} className={styles.icon} />
+              Treasures
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={infoSquare} className={styles.icon} />
+              Help
+            </a>
+          </li>
         </ul>
       </div>
     </div>
