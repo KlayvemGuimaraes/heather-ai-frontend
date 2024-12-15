e069602e6a3773f018eecbe62c0e6c57d766d220
rayssabuarque <brayssa17@gmail.com>
2024-12-14 22:05:38 -0300

    fix: bars in home

--- Pleasant/src/pages/Home/Home.tsx
+++ Pleasant/src/pages/Home/Home.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import styles from './Home.module.css'
 import Sidebar from '../../components/SideBar/SideBar';
+import TopBar from '../../components/TopBar/TopBar';
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      
+      <div className={styles.sidebar}>
         <Sidebar/>
+      </div>
+      
+      <div className={styles.container_home}>
+        <TopBar/>
+
+        {/* Conteúdo da Home vai aqui */}
+
+      </div>
     </div>
   );
 };
